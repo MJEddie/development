3a2b2ccb284ef408e012ceae951bd44a1c5d7611
Nick <dreadnaughtlin@gmail.com>
2022-12-09 10:11:49 +0800

    implement render product list

--- woworoom/script.js
+++ woworoom/script.js
@@ -5,11 +5,13 @@ const orderBtn = document.querySelector('.orderInfo-btn');
 
 let productList = [];
 
+// get product list
 function getProductList() {
     const URL = `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/products`;
     axios.get(URL)
         .then(function(res) {
-            console.log(res);
+            productList = res.data.products;
+            renderProductList(productList);
         })
         .catch(function(err) {
             console.log(err);
@@ -17,3 +19,19 @@ function getProductList() {
 }
 
 getProductList();
+
+// Render
+function renderProductList(data) {
+    let str = '';
+    data.forEach(list => {
+        str += `<li class="productCard">
+                <h4 class="productType">新品</h4>
+                <img src="${list.images}" alt="">
+                <a href="#" class="addCardBtn" data-id="${list.id}">加入購物車</a>
+                <h3 data-title="${list.title}">${list.title}</h3>
+                <del class="originPrice">NT$ ${toThousand(list.origin_price)}</del>
+                <p class="nowPrice">NT$ ${toThousand(list.price)}</p>
+            </li>`
+    });
+    productWrap.innerHTML = str;
+}
