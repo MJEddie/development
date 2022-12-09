63cceb058a9e39a28b415f978e8d36348746aaeb
Nick <dreadnaughtlin@gmail.com>
2022-12-09 12:11:37 +0800

    implement shopping cart function

--- woworoom/script.js
+++ woworoom/script.js
@@ -12,6 +12,8 @@ function init() {
 init();
 
 // get product list
+let productList = [];
+
 function getProductList() {
     const URL = `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/products`;
     axios.get(URL)
@@ -24,7 +26,21 @@ function getProductList() {
         })
 }
 
-getProductList();
+let cartList = [];
+let totalPrice;
+
+function getCartList() {
+    const URL = `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts`;
+    axios.get(URL)
+        .then(function(res) {
+            cartList = res.data.carts;
+            totalPrice = res.data.finalTotal;
+            renderCartList(cartList);
+        })
+        .catch(function(err) {
+            console.log(err);
+        })
+}
 
 // Render
 function renderProductList(data) {
@@ -40,6 +56,42 @@ function renderProductList(data) {
             </li>`
     });
     productWrap.innerHTML = str;
+}
+
+function renderCartList(data) {
+    let str = '';
+    data.forEach(list => {
+        str += `<tr>
+        <td>
+        <div class="cardItem-title">
+        <img src="${list.product.images}" alt="">
+        <p>${list.product.title}</p>
+        </div>
+        </td>
+        <td>NT$ ${toThousand(list.product.price)}</td>
+                        <td>${list.quantity}</td>
+                        <td>NT$ ${toThousand(list.product.price * list.quantity)}</td>
+                        <td class="discardBtn">
+                        <a href="#" class="material-icons" data-id="${list.id}">
+                        clear
+                        </a>
+                        </td>
+                        </tr>`
+    });
+    if (cartList.length > 0) {
+        str += `<tr>
+                        <td>
+                            <a href="#" class="discardAllBtn">刪除所有品項</a>
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <p>總金額</p>
+                        </td>
+                        <td>NT$ ${totalPrice}</td>
+                    </tr>`
+    }
+    shoppingCartList.innerHTML = str;
 }
 
 function toThousand(num) {
